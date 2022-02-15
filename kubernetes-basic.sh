b07bfb66a5f329001b1e947f01a8aac06a210537
Jonas Alder <jonas.alder@zuehlke.com>
2022-02-15 19:05:14 +0100

    add more content to kubernetes

--- kubernetes-basic.sh
+++ kubernetes-basic.sh
@@ -2,9 +2,14 @@
 ## Usage of ConfigMap
 ## Usage of Secret (Extended: Ways to manage secrets the real way or why secrets are usually not very secret)
 ## Usage of ENV vars
-## Usage of Service
+## Usage of Service (NodePort, ClusterIP, LoadBalancer)
+## Hint at DNS
+## Explain K8s architecture and essential components (ETCD, Kube API, Kube Controller, Kube Scheduler, Kube Proxy, Kubelet, ...)
+## Hint at CPU/Memory resources request/limits?
+## Hint at Quotas (CPU/memory/pods)
+## Hint at RBAC
 ## Explain & Usage of Ingress?
-
+## What the heck is YAML? Can't I just use JSON? (yes you can)
 
 # Run pod simple
 kubectl run pod nginx --image nginx
@@ -23,8 +28,22 @@ kubectl get pod nginx -o yaml
 # kubectl CLI in detail
 ## How to even pronounce?
 ## Types/groups of commands
+## Short names
+kubectl api-resources
 ## imperative vs. declarative / create vs. apply
-## 
+kubectl help
+# Also see https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands
+# and https://kubernetes.io/docs/reference/kubectl/conventions/
+
+kubectl run nginx-run --image=nginx
+kubectl run nginx --image=nginx --dry-run=client -o yaml
+kubectl create deployment --image=nginx nginx
+kubectl update ...
+kubectl edit deployment nginx
+
+kubectl apply
+
+## Should not nix imperative/declarative commands (due to last-applied config)
 
 # Scale deployment
 # Change deployment to see rolling update
@@ -43,6 +62,13 @@ kubectl get pod nginx -o yaml
 # Create pod with same name in new namespace
 ## Hint at tools to manage context?
 ## Hint at ~/.kube
+kubectl create namespace
+kubectl config set-context $(kubectl config current-context) --namespace=dev
+kubectl get pods
+kubectl get pods --namespace=default 
+kubectl get pods --all-namespaces 
+# List "everything" in the cluster, if permissions allow it
+kubectl get all --all-namespaces 
 
 # Helm
 helm install X

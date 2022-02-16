c65fc4b57e2d25146585f27872f9d7ebc7748ccd
Adrian Damm <adriandamm@gmail.com>
2022-02-16 21:36:14 +0100

    Extended Docker content.

--- docker-basic1.sh
+++ docker-basic1.sh
@@ -2,6 +2,10 @@
 #Docker Part 1
 #Why Docker & use cases (local development, in a cluster, etc.), understanding
 
+# get the runtime version 
+docker version
+
+# 1st hello world
 docker run hello-world
 
 # explain command, action, how theses commands are used. 
@@ -37,12 +41,45 @@ docker run ubuntu cat /etc/*release*
 
 # explain volume with -v, https://docs.docker.com/storage/volumes/ 
 
-# Exercise
-docker run -d --restart always -p 81:80 -m 64m --name a5 -v /home/adamm/test:/home nginx:latest
-
 # Show some examples how to run an application to try it:
 #$ docker run --name some-wordpress -p 8080:80 -d wordpress => https://hub.docker.com/_/wordpress
 
 # Cleanup 
 docker system prune -f
 
+# excericese
+# which docker version
+docker version
+# how many containers are running
+docker ps 
+# How many containers are PRESENT on the host now? Including both Running and Not Running ones
+docker ps  -a
+#How many ports are published on this container?=> Run the command docker ps and look under the PORTS column.
+docker ps  
+#Which of the below ports are the exposed on the CONTAINER?
+#Which of the below ports are published on Host?
+
+
+
+# how many images are available
+docker images
+#What is the size of the ubuntu image?
+docker images 
+#What is the tag of the image?
+docker images 
+# what is the name of the container with image ubuntu
+docker ps  -a
+#What is the ID of the container that uses the alpine image and is not running?
+# run a redis image
+docker run -d redis
+# stop the image you have just created
+docker stop <container-id> 
+#What is the image used to run the nginx-1 container?
+docker ps  -a
+#Delete all containers from the Docker Host. Both Running and Not Running ones. Remember you may have to stop containers before deleting them.
+ #Pull the image nginx:1.14-alpine
+docker pull nginx:1.14-alpine
+# Run a container with the nginx:1.14-alpine image and name it webapp
+docker run -d --name webapp nginx:1.14-alpine
+# Cleanup: Delete all images on the host Remove containers as necessary
+docker rmi $(docker images -aq)
--- docker-basic3.sh
+++ docker-basic3.sh
@@ -5,5 +5,21 @@
 # Link: https://docs.docker.com/compose/compose-file/
 # Good example https://github.com/dockersamples/example-voting-app
 
-# Idea: First crate two container like a clickcounter(kodekloud/click-counter) and a redis. # explain link (--link <name or id>:alias) option and that it is deprecated, 
-# Afterwards create your own compose file. see folder compose.
+
+# Excercise
+#First create a redis database container called redis, image redis:alpine. 
+docker run --name redis -d redis:alpine
+
+#Next, create a simple container called clickcounter with the image kodekloud/click-counter, link it to the redis container that we created in the previous task and then expose it on the host port 8085
+#The clickcounter app run on port 5000. 
+docker run -d --name=clickcounter --link redis:redis -p 8085:5000 kodekloud/click-counter
+# You can now access this application using the Click-Counter tab above the terminal. Refresh the page and see the count increase.
+# Let's clean up the actions carried out in previous steps. Delete the redis and the clickcounter containers.
+#To stop the containers: docker stop <CONTAINER-NAME>
+#To delete the containers: docker rm <CONTAINER-NAME>
+
+#Create a docker-compose.yml file under the directory /root/clickcounter. Once done, run docker-compose up.
+
+#The compose file should have the exact specification as follows - redis service specification - Image name should be redis:alpine.
+#clickcounter service specification - Image name should be kodekloud/click-counter, app is run on port 5000 and expose it on the host port 8085 in the compose file.
+
